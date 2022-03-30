fc36dcade6c8a087f0f6c6e2f46226ae7f1c36b7
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-03-30 21:11:30 +0200

    finished the first lecture of feedback project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from "./components/Header"
+
+
 function App() {
   return (
+    <>
+    <Header text={true} />
     <div className="container">
       <h1>My App</h1>
     </div>
+    </>
   )
 }
 
