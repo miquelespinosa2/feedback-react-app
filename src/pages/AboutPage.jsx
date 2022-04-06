e8214e43cb5786dcfd27a745980517e7cef7bdc9
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-06 15:57:16 +0200

    finished the pending lesson

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import Card from '../components/shared/Card'
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <Card className='about'>
+      <h1>About this project</h1>
+      <p>This is a React app to leave feedback for a product
+        or service
+      </p>
+      <p>Versio: 1.0.0</p>
+    </Card>
+  )
+}
+
+export default AboutPage
