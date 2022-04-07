e930c3be9bb024611b41c78541ac240f8e3b1734
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-07 16:05:31 +0200

    refactored into context the handleDelete function

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -12,9 +12,16 @@ export const FeedbackProvider = ({children}) => {
     },
   ])
 
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
 
   return <FeedbackContext.Provider value={{
     feedback,
+    deleteFeedback,
   }}>
     {children}
   </FeedbackContext.Provider>
