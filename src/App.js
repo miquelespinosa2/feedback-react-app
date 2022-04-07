c86571821e2e8499307ff471b8e87f851a33dac0
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-07 15:58:53 +0200

    first slessons of the 6th section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
+import {FeedbackProvider} from './context/FeedbackContext'
 
 
 function App() {
@@ -25,26 +26,28 @@ function App() {
     }
   }
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-        <Route exact path='/' element={
-          <>
-          <FeedbackForm handleAdd={addFeedback}/>
-          <FeedbackStats feedback={feedback}/>
-          <FeedbackList feedback={feedback}
-          handleDelete={deleteFeedback} />
-          </>
-        }>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+          <Route exact path='/' element={
+            <>
+            <FeedbackForm handleAdd={addFeedback}/>
+            <FeedbackStats feedback={feedback}/>
+            <FeedbackList feedback={feedback}
+            handleDelete={deleteFeedback} />
+            </>
+          }>
 
-        </Route>
+          </Route>
 
-      <Route path='/about' element={<AboutPage />} />
-      </Routes>
-      <AboutIconLink />
-      </div>
-    </Router>
+        <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
