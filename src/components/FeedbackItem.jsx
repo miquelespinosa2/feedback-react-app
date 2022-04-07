c6386eb959529962df0a4583880126806d944356
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-07 16:15:14 +0200

    fixed the deletion button bug

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import {useContext} from 'react'
 import { useState } from 'react'
@@ -10,8 +10,11 @@ function FeedbackItem({ item }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => deleteFeedback(item.id) }>
+      <button onClick={() => deleteFeedback(item.id) } className='close'>
         <FaTimes color='purple' />
+      </button>
+      <button className="edit">
+        <FaEdit color='purple'/>
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
